534db89e03121b754e015bcf55acd475a9d38fbb
Zhao Zhang <626788149@qq.com>
2016-05-12 14:59:16 +0800

    bytec

--- src/csopcodes.c
+++ src/csopcodes.c
@@ -1,9 +1,24 @@
 #include "csopcodes.h"
 
 static char *csO_opnames[NUM_OPCODES+1] = {
-	
+	"MOVE",
+	"LOADCONST",
+	"ADD",
+	"SUB",
+	"MUL",
+	"DIV",
+	"AND",
+	"OR",
+	"NOT",
+	"CONCAT",
+	"CALL",
+	"JMP",
+	"EQ",
+	"LT",
+	"LE",
+	NULL	
 };
 
 static csG_byte csO_opmodes[NUM_OPCODES] = {
-	
+
 };
